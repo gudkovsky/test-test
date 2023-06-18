d0e51b990d54d9975e1d59a2188e599cdcace1f6
Gudkov <gudkov.id@mail.ru>
2023-06-18 22:48:49 +0300

    fix: browser to hash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import StartPage from './components/pages/start-page/StartPage.jsx';
 import FormPage from './components/pages/form-page/FormPage.jsx';
 import LayoutForm from "./components/blocks/layout-form/LayoutForm.jsx";
@@ -7,16 +7,16 @@ import LayoutForm from "./components/blocks/layout-form/LayoutForm.jsx";
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/">
           <Route index element={<StartPage />}/>
-          <Route path="/create" element={<LayoutForm />}>
+          <Route path="create" element={<LayoutForm />}>
             <Route index element={<FormPage/>}/>
           </Route>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     )
   }
 export default App;
